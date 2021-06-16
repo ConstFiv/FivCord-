d5af1fdf8069b3ddaa5cbd5fef5618358196a965
ConstFiv <710144136@qq.com>
2021-06-16 15:49:48 +0800

    2021-6-16

---
+++ chat/FriendsList/FriendsList.js
@@ -0,0 +1,14 @@
+
+
+let listUl = document.querySelectorAll("#listUl li");
+for(let i =0;i<listUl.length;i++){
+    console.log(allFriends.Curtain.name);
+}
+// for(let i =0;i<listUl.length;i++){
+//     listUl[i].innerHTML = "<div class='imgs'> <span class='iconfont "+allFriends.i.imgs+"'></span>\
+//     </div>\
+//     <span>"+allFriends.i.name+"</span>"
+    
+// }
+
+console.log(listUl);
---
+++ chat/FriendsList/FriendsListData.js
@@ -0,0 +1,29 @@
+let listA = ["Curtain","blank","不吃香菜的皮皮椒","陈青"];
+
+
+let allFriends = {
+    "Curtain":{
+        name:"Curtain",
+        imgs :"icon-dongwutubiao-shangse-huli",
+        color:"yellow"
+    },
+    "blank":{
+        name:"blank",
+        imgs :".icon-dongwutubiao-shangse-laoshu",
+        color:"purple"
+    },
+    "不吃香菜的皮皮椒":{
+        name:"不吃香菜的皮皮椒",
+        imgs :"icon-dongwu",
+        color:"green"
+    },
+    "陈青":{
+      
+            name:"陈青",
+            imgs :"icon-xiaojidongwuniao",
+            color:"blue"
+       
+    }
+
+    
+}
