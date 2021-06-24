dc19544c0983fbed4a8111acfe2cf4a4aeb4048e
ConstFiv <710144136@qq.com>
2021-06-24 14:30:20 +0800

    解决了多行文字重叠的bug

---
+++ 0.1.2.2 修复了大批量文字发送时重叠的bug/chat/FriendsList/FriendsListData.js
@@ -0,0 +1,149 @@
+
+
+
+let listA = ["Curtain","blank","不吃香菜的皮皮椒","陈青","橘猫","Orient\
+Ink","广寒宫上喂兔子的","咸鱼王","山角函兽","真正的发芽土豆","NtB","Iccccc","七星","花辞树","Sakura","我很老实","入海"];
+
+
+let allFriends = {
+    "Curtain":{
+        name:"Curtain",
+        imgs :"icon-dongwutubiao-shangse-huli",
+        color:"rgb(148,95,187)",
+        fontSize:"35rem",
+    },
+    "blank":{
+        name:"blank",
+        imgs :"icon-dongwutubiao-shangse-laoshu",
+        color:"rgb(20,181,227)",
+        fontSize:"35rem",
+    },
+    "不吃香菜的皮皮椒":{
+        name:"不吃香菜的皮皮椒",
+        imgs :"icon-dongwu",
+        color:"rgb(201,211,96)",
+        fontSize:"20rem",
+    },
+    "陈青":{
+      
+            name:"陈青",
+            imgs :"icon-xiaojidongwuniao",
+            color:"rgb(242,196,99)",
+            fontSize:"20rem",
+       
+    },
+
+    "橘猫":{
+      
+        name:"橘猫",
+        imgs :"icon-dongwutubiao-shangse-ma",
+        color:"rgb(201,211,96)",
+        fontSize:"35rem",
+   
+    },
+
+    "OrientInk":{
+      
+        name:"OrientInk",
+        imgs :"icon-dongwutubiao-shangse-daxiang",
+        color:"rgb(148,95,187)",
+        fontSize:"35rem",
+   
+    },
+
+    "广寒宫上喂兔子的":{
+      
+        name:"广寒宫上喂兔子的",
+        imgs :"icon-dongwutubiao-shangse-changjinglu",
+        color:"rgb(242,196,99)",
+        fontSize:"35rem",
+   
+    },
+
+    "咸鱼王":{
+      
+        name:"咸鱼王",
+        imgs :"icon-dongwutubiao-shangse-shizi",
+        color:"rgb(20,181,227)",
+        fontSize:"35rem",
+   
+    },
+  
+    "山角函兽":{
+      
+        name:"山角函兽",
+        imgs :"icon-dongwutubiao-shangse-laohu",
+        color:"rgb(242,196,99)",
+        fontSize:"35rem",
+   
+    },
+    "真正的发芽土豆":{
+      
+        name:"真正的发芽土豆",
+        imgs :"icon-mao",
+        color:"rgb(20,181,227)",
+        fontSize:"35rem",
+   
+    },
+    "NtB":{
+      
+        name:"NtB",
+        imgs :"icon-shenghuafangju",
+        color:"rgb(148,95,187)",
+        fontSize:"35rem",
+   
+    },
+
+    "Iccccc":{
+      
+        name:"Iccccc",
+        imgs :"icon-dongwutubiao-shangse-changjinglu",
+        color:"rgb(242,196,99)",
+        fontSize:"35rem",
+   
+    },
+
+    "七星":{
+      
+        name:"七星",
+        imgs :"icon-dongwutubiao-shangse-shizi",
+        color:"rgb(201,211,96)",
+        fontSize:"35rem",
+   
+    },
+
+    "花辞树":{
+      
+        name:"花辞树",
+        imgs :"icon-dongwutubiao-shangse-daxiang",
+        color:"rgb(20,181,227)",
+        fontSize:"35rem",
+   
+    },
+    "Sakura":{
+      
+        name:"Sakura",
+        imgs :"icon-dongwu",
+        color:"rgb(242,196,99)",
+        fontSize:"20rem",
+   
+    },
+    "我很老实":{
+      
+        name:"我很老实",
+        imgs :"icon-dongwutubiao-shangse-laoshu",
+        color:"rgb(148,95,187)",
+        fontSize:"35rem",
+   
+    },
+    "入海":{
+      
+        name:"入海",
+        imgs :"icon-dongwutubiao-shangse-changjinglu",
+        color:"rgb(201,211,96)",
+        fontSize:"35rem",
+   
+    },
+
+    
+}
